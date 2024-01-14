926750557563bfc8f826f8bd49dc2862e69a75f5
saelani <saelani@gmail.com>
2024-01-14 09:35:00 +0700

    feat: gabung file react ke express

---
+++ public/precache-manifest.970c0de472d75e944e6e13af818bb60b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be9c38e36b48960ab0995d0efa5aac28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed729c55ab5d99a561f1",
+    "url": "/static/css/2.e9005a29.chunk.css"
+  },
+  {
+    "revision": "fd1e036e4abb6317bc23",
+    "url": "/static/css/main.9502da91.chunk.css"
+  },
+  {
+    "revision": "ed729c55ab5d99a561f1",
+    "url": "/static/js/2.9f1c6f48.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.9f1c6f48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd1e036e4abb6317bc23",
+    "url": "/static/js/main.e67f8188.chunk.js"
+  },
+  {
+    "revision": "717be7891cc3d466bc4e",
+    "url": "/static/js/runtime-main.b43b082e.js"
+  }
+]);
