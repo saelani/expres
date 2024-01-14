c118c850a67c69aa051ed634edb4afa8e5be42a9
saelani <saelani@gmail.com>
2024-01-14 09:06:48 +0700

    feat: express sucess

---
+++ database/index.js
@@ -0,0 +1,26 @@
+// (1) import package mongoose
+const mongoose = require('mongoose');
+
+// (2) kita import konfigurasi terkait MongoDB dari `app/config.js`
+const { dbHost, dbName, dbPort, dbUser, dbPass } = require('../app/config');
+
+// (3) connect ke MongoDB menggunakan konfigurasi yang telah kita import 
+//mongodb+srv://saelani:<password>
+//@cluster0.llapuef.mongodb.net/?retryWrites=true&w=majority
+
+mongoose
+  .connect(`mongodb://${dbUser}:${dbPass}@${dbHost}/${dbName}?authSource=admin`, 
+    {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+
+// mongoose
+// .connect(`mongodb+srv://saelani:@Bandung1@cluster0.llapuef.mongodb.net/homestore?retryWrites=true&w=majority`,
+// {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+
+
+// (4) simpan koneksi dalam constant `db`
+const db =
+  mongoose.connection;
+
+// (5) export `db` supaya bisa digunakan oleh file lain yang membutuhkan
+module.exports = db;
+
